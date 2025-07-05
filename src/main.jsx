c006d643bdb70a48b654f99c5b8e6bf57abd9cbd
khai <khaitruong1301@gmail.com>
2025-07-05 17:28:36 +0700

    buoi1_component_databinding_handleEvent

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,20 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import BT1_HomeComponent from './components/BT1_HomeComponent'
+//import css
+import "./css/index.css";
+import DataBinding from './databinding/DataBinding';
+import HandleEvent from './HandleEvent/HandleEvent';
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+   {/* <BT1_HomeComponent /> */}
+   {/* <DataBinding /> */}
+   <HandleEvent />
+  
+  </>
 )
+
+
+
